34ee78f248dc979d4bfc712ad314d4b780f6ba04
alymu <alym7mdd@gmail.com>
2025-02-03 16:57:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.retrofit
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.text_View)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val retrofit: Retrofit by lazy {
+            Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        val apiInterface = retrofit.create(ApiInterface::class.java)
+        val call : Call<Post> = apiInterface.post
+        call.enqueue(object : Callback<Post>{
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                textView.text = response.body()?.title
+            }
+
+            override fun onFailure(call: Call<Post>, e: Throwable) {
+                textView.text = e.message
+            }
+        })
+    }
+}
